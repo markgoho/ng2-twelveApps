b775f1e4ed0c6cd85aec8f2c870392f89174b622
Mark Goho <markgoho@gmail.com>
2017-01-23 10:23:41 -0500

    Finished simple website

--- ng2website/src/app/app.module.ts
+++ ng2website/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    JumbotronComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
